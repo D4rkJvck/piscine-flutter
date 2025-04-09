f686336e0169bfd86f506315cfc8eacade7d3793
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2025-04-09 09:07:36 +0000

    DART-INTRO_3

---
+++ data-structure.dart
@@ -0,0 +1,7 @@
+List<int> listNum = [1, 2, 3, 4, 5];
+List<Object> listObj = [1, 'a', true, 3.14];
+List<String> listStr = ['apple', 'banana', 'cherry'];
+List<List<Object>> listList = [listNum, listObj, listStr];
+Set<String> setStr = {'apple', 'banana', 'cherry'};
+Map<String, int> mapStr = {'apple': 1, 'banana': 2, 'cherry': 3};
+
