a78122ca0082b775e00485359a210ce5550402fa
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2025-04-09 09:41:53 +0000

    DART-INTRO_9

---
+++ person.dart
@@ -0,0 +1,17 @@
+class Person {
+  String name;
+  String? surname;
+  String cityOfOrigin;
+  int age;
+  int height;
+
+  Person({
+    required this.name,
+    this.surname,
+    required this.cityOfOrigin,
+    required this.age,
+    required this.height,
+  });
+}
+
+
